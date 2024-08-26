0591c3f6cfafc82c2d4a6b46cf814107fde65c89
Max-McRuvie <mcruvie@hotmail.co.uk>
2024-08-26 12:24:40 +0100

    Added configuration header file for game settings

---
+++ include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// screen clearing configuration
+#define USE_CLS_ON_WINDOWS 1
+#define USE_CLEAR_ON_UNIX 1
+
+// game configurations
+#define ROW 8
+#define COL 8
+#define SNAKE_SYMBOL '$'
+#define COLLECTABLE_SYMBOL 'F'
+
+#endif // CONFIG_H
