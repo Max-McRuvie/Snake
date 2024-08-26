c267ff9692696c6c23349d8f22f6139f0e8b99c8
Max-McRuvie <mcruvie@hotmail.co.uk>
2024-08-26 12:24:57 +0100

    Implemented basic game loop

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-
+#include <stdbool.h> 
+#include <windows.h>
+#include "config.h"
+#include "grid.h"
+#include "clear_screen.h"
 
 int main(){
-    printf("Hello World");
+    char grid[ROW][COL];
+    create_border(grid);
+
+    while(true){
+        ClrScr();
+        print_grid(grid);
+
+        Sleep(3000);
+    }
     
     return 0;
 }
